aa629093df19ef2eb0068dd8e9d2a2d905be6334
Ned <nedcoder@gmail.com>
2020-06-07 10:21:46 +0300

    new changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-
+from django.contrib.auth.views import (
+    LoginView,
+)
+from .views import UserLoginView
 
 urlpatterns = [
-
+    # custoimize View behaviour
+    # path('user/login/', LoginView.as_view(), name='user_login'),
+    #path('login/', UserLoginView.as_view(), name='user_login'),
 ]
--- exploredjango/urls.py
+++ exploredjango/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('user/', include('django.contrib.auth.urls')),
     path('user/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
